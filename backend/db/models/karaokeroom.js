65552e31fa45e0773859bfa5ffdb456df63f8cb3
victornoel <vnoel890@gmail.com>
2023-07-11 14:50:46 -0400

    Add room model

---
+++ backend/db/models/karaokeroom.js
@@ -0,0 +1,12 @@
+const {DataTypes} = require("sequelize");
+const db = require('../index');
+
+const Room = db.define("room", {
+    admin: {
+        type: DataTypes.STRING,
+        allowNull:false,
+    },
+    
+})
+
+module.exports = Room;
