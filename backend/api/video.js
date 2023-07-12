596ab2162c99115bade856a3a241a4c4ab58bf93
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-12 13:08:17 -0400

    Add route to post music to video table in database using search term keyword

--- backend/api/video.js
+++ backend/api/video.js
@@ -1,6 +1,21 @@
 const express = require("express");
+// const {gapi} = require("googleapis");
+const youtubesearchapi = require("youtube-search-api");
 const router = express.Router();
 const { Video } = require("../db/models");
+
+router.post("/addmusic/:keyword", async (req, res, next) => {
+    try {
+        const keyword = await req.params.keyword;
+        const searchResult = await youtubesearchapi.GetListByKeyword(keyword);
+        const videoId = searchResult.items[1].id;
+        const videoLink = `https://www.youtube.com/watch?v=${videoId}`;
+        const newVideo = await Video.create({"link": videoLink});
+        res.send(newVideo);
+    } catch (error) {
+        next(error);
+    }
+})
 
 router.get("/", async (req, res, next) => {
     try {
