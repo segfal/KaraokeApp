90cde451d3bd07429760fb3fcaa74daec51353a4
victornoel <vnoel890@gmail.com>
2023-07-12 14:10:52 -0400

    Add handle change for input

--- frontend/src/components/Karaoke/Search/Search.jsx
+++ frontend/src/components/Karaoke/Search/Search.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import { React, useState } from 'react';
 import "./search.css";
 
-// Anytime you do a search it adds to a queue
 const Search = () => {
+  const [ keyword, setKeyword ] = useState("");
+
+  const handleInput = (event) => {
+    setKeyword(event.target.value);
+    console.log(keyword);
+  }
+
   return (
     <div class="col-md-8">
       <div class="d-flex form-inputs">
-        <input class="form-control" onChange={handleInput()} type="text" placeholder="Search for music..."/>
-        <button class="btn-search" onClick={handleSearch()}>
+        <input class="form-control" onChange={handleInput} type="text" placeholder="Search for music..."/>
+        <button class="btn-search">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
           </svg>
