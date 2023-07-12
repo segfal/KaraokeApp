7c2d338593e2674a2e211d904738f1df016493d3
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-12 11:43:53 -0400

    Add get all videos route

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
 router.use("/room", require("./room"));
-// router.use("/video", require("./video"));
+router.use("/video", require("./video"));
 
 module.exports = router;
--- backend/api/video.js
+++ backend/api/video.js
@@ -4,8 +4,13 @@ const { Video } = require("../db/models");
 
 router.get("/", async (req, res, next) => {
     try {
-        
+        const allVideos = await Video.findAll();
+        allVideos
+            ? res.status(200).json(allVideos)
+            : res.status(404).send("No campuses found");
     } catch (error) {
-         next(error);
+        next(error);
     }
 })
+
+module.exports = router;
