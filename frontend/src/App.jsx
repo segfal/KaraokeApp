e4dde012d68c5fa110c8f1929a97b24ba36fea99
victornoel <vnoel890@gmail.com>
2023-07-12 11:10:59 -0400

    Add base components for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from './components/NavBar';
+import Home from './components/Home';
 import './App.css'
+import Room from './components/Karaoke/Room/Room';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/karaoke" element={<Room/>}/>
+      </Routes>
+      
+      </>
+
+    </Router>
+    
   )
 }
 
---
+++ frontend/src/components/Karaoke/Room/Room.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Video from '../Video/Video'
+import Participants from '../Participants/Participants'
+
+// Future: search bar and queue option
+const Room = () => {
+
+  return (
+    <div>
+      <h1>Room</h1>
+    <Video/>
+    <Participants/>
+
+    </div>
+    
+  )
+}
+
+export default Room
