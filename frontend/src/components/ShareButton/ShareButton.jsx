175364ab9671cefef8e301ecba4da2068b881375
victornoel <vnoel890@gmail.com>
2023-07-13 12:47:21 -0400

    Add base templete for queue, and share button, made link embed in backend

---
+++ frontend/src/components/ShareButton/ShareButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import share from '/images/share.png';
+
+
+const ShareButton = () => {
+  return (
+    <div>
+        <button><img alt="share"></img></button>
+    </div>
+    
+  );
+};
+
+export default ShareButton;
