c62e5f95aa77b61d7c9380beda95d82e15e40e7f
Unknown-Pplayer <xwang2230@gmail.com>
2023-07-13 11:11:56 -0400

    room routes

--- backend/api/room.js
+++ backend/api/room.js
@@ -1,17 +1,55 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Room } = require("../db/models");
+const { Room } = require('../db/models');
 
 // Get all Rooms
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const allRooms = await Room.findAll({include: ["participants", "queue"]});
+    const allRooms = await Room.findAll({ include: ['participants', 'queue'] });
     allRooms
       ? res.status(200).json(allRooms)
-      : res.status(404).json("No rooms"); 
+      : res.status(404).json('No rooms');
   } catch (error) {
     next(error);
   }
 });
+//post a new room
+router.post('/', async (req, res, next) => {
+  try {
+    const newRoom = await Room.create(req.body);
+    res.status(201).json(newRoom);
+  } catch (error) {
+    next(error);
+  }
+});
+//delete a room
+router.delete('/:roomId', async (req, res, next) => {
+  try {
+    const room = await Room.findByPk(req.params.roomId);
+    if (room) {
+      await room.destroy();
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+//update a room
+router.put('/:roomId', async (req, res, next) => {
+  try {
+    const room = await Room.findByPk(req.params.roomId);
+    if (room) {
+      await room.update(req.body);
+      res.json(room);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+//get a room by id
 
 module.exports = router;
