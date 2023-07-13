c976c20eb5f8f43a93428b674408ee8c73468369
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-07-13 10:45:39 -0400

    added fetchRoomThunk

--- frontend/src/redux/Room/Room.actions.js
+++ frontend/src/redux/Room/Room.actions.js
@@ -76,5 +76,18 @@ export const setCurrentRoomThunk = (roomId) => {
 }
 
 
+export const fetchRoomThunk = (roomId) => {
+    return async (dispatch) => {
+        try {
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+
+
 
 
