2ba7c9a0d9ac5eb6ca96cb87bf96f8016f5b632e
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-20 11:18:20 -0400

    Create user model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,35 +1,110 @@
-const { DataTypes } = require("sequelize");
+const crypto = require('crypto');
+const { user } = require('pg/lib/defaults');
+const { Model, DataTypes } = require("sequelize");
 const db = require('../db');
 
-const User = db.define("user", {
-  userName: {
-    type: DataTypes.STRING,
-    allowNull: true,
+class User extends Model {
+  static async generateSalt() {
+    return crypto.randomBytes(16).toString("base64");
+  }
+  static async encyrptPassword(pw, salt) {
+    return crypto.createHash('RSA-SHA256').update(pw).update(salt).digest("hex");
+  }
+  static async correctPassword(pwAttempt) {
+    return User.encyrptPassword(pwAttempt, this.salt) === this.password;
+  }
+}
+
+User.init(
+  {
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      // unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    salt: {
+      type: DataTypes.STRING,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    lastName: {
+      type : DataTypes.STRING,
+      allowNull: true
+    },
+    profilePic: { 
+      type: DataTypes.STRING, // will change to Files using a profilePic database
+      allowNull: true,
+    },  
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,  
+      // unique: true
+    },
+    googleId: { // OAuth
+      type: DataTypes.STRING
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  lastName: {
-    type : DataTypes.STRING,
-    allowNull: true
-  },
-  profilePic: { 
-    type: DataTypes.STRING, // will change to Files using a profilePic database
-    allowNull: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,  
-  },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-});
+  {
+    sequelize: db,
+    modelName: "User",
+    hook: {
+      beforeSave: async (user) => {
+        if (user.changed('password')){
+          user.salt = await User.generateSalt();
+          user.password = await User.encyrptPassword(user.password, user.salt);
+        }
+      },
+      beforeBulkCreate: async (users) => {
+        users.forEach(async (user) => {
+          if (user.changed("password")) {
+            user.salt = await User.generateSalt();
+            user.password = await User.encyrptPassword(user.password, user.salt);
+          }
+        })
+      }
+    }
+  }
+)
+
+// const User = db.define("user", {
+//   userName: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+//   password: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+//   firstName: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+//   lastName: {
+//     type : DataTypes.STRING,
+//     allowNull: true
+//   },
+//   profilePic: { 
+//     type: DataTypes.STRING, // will change to Files using a profilePic database
+//     allowNull: true,
+//   },  
+//   email: {
+//     type: DataTypes.STRING,
+//     allowNull: false,  
+//   },
+//   role: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+// });
 
 module.exports = { User };
