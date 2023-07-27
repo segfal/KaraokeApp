6cfa924e563e9c207a818bf74d763cb8c1917f84
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-27 13:31:50 -0400

    Fix navbar alignment for log out

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -41,18 +41,22 @@ const Navbar = () => {
             <Link id="home" to={isAuthenticated ? "/profile" : "/"} className="text-left text-mainWhite font-extra-extrabold hover:underline mr-4" style={{fontStyle:"normal"}}>HOME</Link>
           </i>
         </div>
-        <div>
-          <i>
-            {isAuthenticated && (<button onClick={handleLogout} className="text-right font-extra-extrabold  hover:underline text-mainWhite p-2">LOG OUT</button>)}
-          </i>
-        </div>
-        <div>
-          <i>
-            {!isAuthenticated && (<button onClick={handleLogin} className="text-right font-extra-extrabold  hover:underline text-mainWhite p-2 mr-4">LOG IN</button>)}
-          </i>
-          <i>
-            {!isAuthenticated && (<button onClick={handleSignup} className="text-right font-extra-extrabold bg-mainYellow rounded-md hover:bg-mainWhite transition-colors duration-200 ease-in-out p-2">SIGN UP</button>)}
-          </i>
+        <div className="flex items-center">
+          {isAuthenticated && (
+            <i>
+              <button onClick={handleLogout} className="text-right font-extra-extrabold  hover:underline text-mainWhite p-2">LOG OUT</button>
+            </i> 
+          )}
+          {!isAuthenticated && (
+            <>
+              <i>
+                <button onClick={handleLogin} className="text-right font-extra-extrabold hover:underline text-mainWhite p-2 mr-4">LOG IN</button>
+              </i>
+              <i>
+                <button onClick={handleSignup} className="text-right font-extra-extrabold bg-mainYellow rounded-md hover:bg-mainWhite transition-colors duration-200 ease-in-out p-2">SIGN UP</button>
+              </i>
+            </>
+          )}
         </div>
       </nav>
     </div>
