5357c878b1fc0f6ef8e38523391e4ddfa14d85d4
victornoel <vnoel890@gmail.com>
2023-07-27 12:32:43 -0400

    Change url

--- frontend/src/Context.tsx
+++ frontend/src/Context.tsx
@@ -1,7 +1,10 @@
 import React, { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const socket = io('http://localhost:4000'),
+// const backend_url = import.meta.env.VITE_BACKEND_URL
+
+// const socket = io("http://localhost:4000"),
+const socket = io("karaoke-backend-production.up.railway.app"),
 SocketContext = createContext<Socket>(socket);
 
 socket.on('connect', () => console.log('connected to socket'));
