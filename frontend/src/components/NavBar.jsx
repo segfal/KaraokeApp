f32d2830c37435d592d5f229df9f52976d307075
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-27 11:21:40 -0400

    Add nav bar styling with tailwind

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React,{useContext, useState} from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../App";
 import axios from "axios";
+import logo from "../assets/logo.png"
 // Logo and user profile is also supposed to display
 
 
@@ -33,19 +34,24 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav>
-        <div className="bg-mainGreen">
+      <nav className="bg-mainGreen font-montserrat font-extra-bold p-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={logo} alt="eKaraoke" className="h-10 w-10 mr-4"></img>
           <i>
-            <Link id="home" to={isAuthenticated ? "/profile" : "/"} className="text-left text-mainWhite ">HOME</Link>
+            <Link id="home" to={isAuthenticated ? "/profile" : "/"} className="text-left text-mainWhite font-extra-extrabold hover:underline mr-4" style={{fontStyle:"normal"}}>HOME</Link>
+          </i>
+        </div>
+        <div>
+          <i>
+            {isAuthenticated && (<button onClick={handleLogout} className="text-right font-extra-extrabold  hover:underline text-mainWhite p-2">LOG OUT</button>)}
+          </i>
+        </div>
+        <div>
+          <i>
+            {!isAuthenticated && (<button onClick={handleLogin} className="text-right font-extra-extrabold  hover:underline text-mainWhite p-2 mr-4">LOG IN</button>)}
           </i>
           <i>
-            {isAuthenticated && (<button onClick={handleLogout} className="text-right">LOG OUT</button>)}
-          </i>
-          <i>
-            {!isAuthenticated && (<button onClick={handleLogin} className="text-right">LOG IN</button>)}
-          </i>
-          <i>
-            {!isAuthenticated && (<button onClick={handleSignup} className="text-right">SIGN UP</button>)}
+            {!isAuthenticated && (<button onClick={handleSignup} className="text-right font-extra-extrabold bg-mainYellow rounded-md hover:bg-mainWhite transition-colors duration-200 ease-in-out p-2">SIGN UP</button>)}
           </i>
         </div>
       </nav>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,21 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      mainGreen: '#004643',
-      mainWhite: '#fffffe',
-      mainYellow: '#f9bc60',
+    extend: {
+      colors: {
+        mainGreen: '#004643',
+        mainWhite: '#fffffe',
+        mainYellow: '#f9bc60',
+      },
+      fontWeight: {
+        'extra-light': 100,
+        'extra-normal': 200,
+        'extra-medium': 300,
+        'extra-semibold': 400,
+        'extra-bold': 500,
+        'extra-extrabold': 600,
+        'extra-black': 700,
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+      },
     },
-    fontWeight: {
-      'extra-light': 100,
-      'extra-normal': 200,
-      'extra-medium': 300,
-      'extra-semibold': 400,
-      'extra-bold': 500,
-      'extra-extrabold': 600,
-      'extra-black': 700,
-    },
-    extend: {},
   },
   plugins: [],
 };
