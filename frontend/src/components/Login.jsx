5bd4934f2d208da3adf2483c7126f0814078d399
Unknown-Pplayer <xwang2230@gmail.com>
2023-07-28 11:26:30 -0400

    fix nav bar home route when in room

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,66 +1,92 @@
-import React,{useState,useEffect,useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import { useNavigate, Link } from "react-router-dom";
-import {useDispatch, useSelector} from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { setUserThunk } from '../redux/User/User.action';
 import { AuthContext } from '../App';
 
-const Login = () => {
-    const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const data = useSelector((state) => state.user.singleUser);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+const Login = ({ setUserId }) => {
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const data = useSelector((state) => state.user.singleUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        axios.post(`https://karaoke-backend-exp-production.up.railway.app/auth/login`,{email: email, password: password})
-        .then((user)=>{
-            console.log("SUBMIT RES FOR USER: ", user);
-            dispatch(setUserThunk({firstName: user.data.firstName, lastName: user.data.lastName, email: user.data.email, id: user.data.id}));
-            setIsAuthenticated(true);
-            navigate(`/profile/${user.data.id}}`);
-        })
-        .catch((err)=>{
-            console.log("SUBMIT LOGIN ERROR: ", err);
-        })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        `https://karaoke-backend-exp-production.up.railway.app/auth/login`,
+        { email: email, password: password }
+      )
+      .then((user) => {
+        console.log('SUBMIT RES FOR USER: ', user);
+        dispatch(
+          setUserThunk({
+            firstName: user.data.firstName,
+            lastName: user.data.lastName,
+            email: user.data.email,
+            id: user.data.id,
+          })
+        );
+        setIsAuthenticated(true);
+        setUserId(user.data.id);
+        navigate(`/profile/${user.data.id}}`);
+      })
+      .catch((err) => {
+        console.log('SUBMIT LOGIN ERROR: ', err);
+      });
+  };
 
-    return (
-        <div className='bg-lightGreen font-montserrat h-[calc(100vh-208px)] pt-16 flex flex-col items-center justify-center'>
-            <h1 className='text-4xl pb-12 font-extra-extrabold'>Good to see you again ♫</h1>
-            <div className='flex flex-col justify-center items-center font-montserrat rounded-lg py-6 shadow px-10 mx-auto bg-mainGreen w-1/3'>
-                <h1 className='font-semi-bold text-mainWhite text-lg'>Log into an existing account</h1>
-                {/* /auth/login */}
-                <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center"> 
-                    <input type="email" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email" name="email" className="rounded p-2 mt-4 w-64"></input>
-                    <input type="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password" name="password" className="rounded p-2 my-4 w-64"></input>
-                    <button type="submit" className="text-right font-extra-extrabold bg-mainYellow rounded-md hover:bg-mainWhite transition-colors duration-200 ease-in-out p-2">LOG IN</button>
-                </form>
-                {/* <p className='text-mainWhite'>OR</p> */}
+  return (
+    <div className="bg-lightGreen font-montserrat h-[calc(100vh-208px)] pt-16 flex flex-col items-center justify-center">
+      <h1 className="text-4xl pb-12 font-extra-extrabold">
+        Good to see you again ♫
+      </h1>
+      <div className="flex flex-col justify-center items-center font-montserrat rounded-lg py-6 shadow px-10 mx-auto bg-mainGreen w-1/3">
+        <h1 className="font-semi-bold text-mainWhite text-lg">
+          Log into an existing account
+        </h1>
+        {/* /auth/login */}
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center justify-center">
+          <input
+            type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="Email"
+            name="email"
+            className="rounded p-2 mt-4 w-64"></input>
+          <input
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="Password"
+            name="password"
+            className="rounded p-2 my-4 w-64"></input>
+          <button
+            type="submit"
+            className="text-right font-extra-extrabold bg-mainYellow rounded-md hover:bg-mainWhite transition-colors duration-200 ease-in-out p-2">
+            LOG IN
+          </button>
+        </form>
+        {/* <p className='text-mainWhite'>OR</p> */}
 
-                <Link to="/signup" className="text-mainWhite hover:text-gray-500 hover:underline transition-all text-sm mt-4">Create an account</Link>
-                
-            </div>
-        </div>
-    )
-}
+        <Link
+          to="/signup"
+          className="text-mainWhite hover:text-gray-500 hover:underline transition-all text-sm mt-4">
+          Create an account
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // import React, { useState } from 'react';
 // import axios from 'axios';
@@ -90,7 +116,7 @@ export default Login;
 //         <div>
 //             <h1>LOG IN</h1>
 //             {/* /auth/login */}
-//             <form onSubmit={handleSubmit}> 
+//             <form onSubmit={handleSubmit}>
 //                 <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email" name="email" />
 //                 <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" name="password" />
 //                 <button type="submit">Log in</button>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,36 +1,36 @@
-import React,{useContext, useState} from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { AuthContext } from "../App";
-import axios from "axios";
-import logo from "../assets/logo.png"
+import React, { useContext, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { AuthContext } from '../App';
+import axios from 'axios';
+import logo from '../assets/logo.png';
 // Logo and user profile is also supposed to display
 
-
-const Navbar = () => {
+const Navbar = ({ userId }) => {
   const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     //using try catch to handle errors
-    try{
-        //axios call to logout
-        const res = await axios.post(`http://https://karaoke-backend-exp-production.up.railway.app/auth/logout`);
-        console.log("LOGOUT RES: ", res);
-        setIsAuthenticated(false);
-        navigate(`/`);
+    try {
+      //axios call to logout
+      const res = await axios.post(
+        `http://https://karaoke-backend-exp-production.up.railway.app/auth/logout`
+      );
+      console.log('LOGOUT RES: ', res);
+      setIsAuthenticated(false);
+      navigate(`/`);
+    } catch (err) {
+      console.log('LOGOUT ERROR: ', err);
     }
-    catch(err){
-        console.log("LOGOUT ERROR: ", err);
-    }
-  }
+  };
 
   const handleLogin = () => {
-    navigate("/login");
-  }
+    navigate('/login');
+  };
 
   const handleSignup = () => {
-    navigate("/signup");
-  }
+    navigate('/signup');
+  };
 
   return (
     <div>
@@ -38,22 +38,40 @@ const Navbar = () => {
         <div className="flex items-center">
           <img src={logo} alt="Serenade" className="h-10 w-10 mr-4"></img>
           <i>
-            <Link id="home" to={isAuthenticated ? "/profile" : "/"} className="text-left text-mainWhite font-extra-extrabold hover:underline mr-4" style={{fontStyle:"normal"}}>HOME</Link>
+            <Link
+              id="home"
+              to={isAuthenticated ? `/profile/${userId}` : '/'}
+              className="text-left text-mainWhite font-extra-extrabold hover:underline mr-4"
+              style={{ fontStyle: 'normal' }}>
+              HOME
+            </Link>
           </i>
         </div>
         <div className="flex items-center">
           {isAuthenticated && (
             <i>
-              <button onClick={handleLogout} className="text-right font-extra-extrabold  hover:underline text-mainWhite p-2">LOG OUT</button>
-            </i> 
+              <button
+                onClick={handleLogout}
+                className="text-right font-extra-extrabold  hover:underline text-mainWhite p-2">
+                LOG OUT
+              </button>
+            </i>
           )}
           {!isAuthenticated && (
             <>
               <i>
-                <button onClick={handleLogin} className="text-right font-extra-extrabold hover:underline text-mainWhite p-2 mr-4">LOG IN</button>
+                <button
+                  onClick={handleLogin}
+                  className="text-right font-extra-extrabold hover:underline text-mainWhite p-2 mr-4">
+                  LOG IN
+                </button>
               </i>
               <i>
-                <button onClick={handleSignup} className="text-right font-extra-extrabold bg-mainYellow rounded-md hover:bg-mainWhite transition-colors duration-200 ease-in-out p-2">SIGN UP</button>
+                <button
+                  onClick={handleSignup}
+                  className="text-right font-extra-extrabold bg-mainYellow rounded-md hover:bg-mainWhite transition-colors duration-200 ease-in-out p-2">
+                  SIGN UP
+                </button>
               </i>
             </>
           )}
