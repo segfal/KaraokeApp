77a4b09ab2c2e50e49b4df62c54c0c02e64f10bd
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-30 01:26:14 -0400

    Style mute and unmute mic

--- frontend/src/components/Karaoke/UserVideo/UserVideo.jsx
+++ frontend/src/components/Karaoke/UserVideo/UserVideo.jsx
@@ -6,6 +6,7 @@ const UserVideo = ({ socket, peer }) => {
   const [userStream, setUserStream] = useState();
   const [peers, setPeers] = useState({});
   const [mediaReady, setMediaReady] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
 
 
   useEffect(() => {
@@ -83,6 +84,7 @@ const UserVideo = ({ socket, peer }) => {
   }, []);
 
   const handleMute = () => {
+    !isMuted ? setIsMuted(true) : setIsMuted(false);
     userStream.getAudioTracks()[0].enabled = !userStream.getAudioTracks()[0].enabled;
   };
 
@@ -94,7 +96,18 @@ const UserVideo = ({ socket, peer }) => {
   return (
     <div id="video-group">
       <video className="user-vid" ref={video} autoPlay muted={true} ></video>
-      <button onClick={handleMute}>Mute Audio</button>
+      <button onClick={handleMute}>
+        {!isMuted ? (
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16">
+            <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/>
+            <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
+          </svg>)
+        : (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-mic-mute-fill" viewBox="0 0 16 16">
+          <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879L5.158 2.037A3.001 3.001 0 0 1 11 3z"/>
+          <path d="M9.486 10.607 5 6.12V8a3 3 0 0 0 4.486 2.607zm-7.84-9.253 12 12 .708-.708-12-12-.708.708z"/>
+        </svg>)
+        }
+      </button>
       <button onClick={handleVideo}>Start Video</button>
       {Object.keys(peers).map((peerId, i) => (
         <SingleUserVideo key={i} videoStream={peers[peerId]} />
